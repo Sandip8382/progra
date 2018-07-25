7b9bd9b516d038cd4f916b9f60a9f3d83d97f266
Sandip8382 <40880777+Sandip8382@users.noreply.github.com>
2018-07-25 10:10:17 +0530

    Add files via upload

---
+++ SMSsender.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net;
+
+
+/// <summary>
+/// Summary description for SMSsender
+/// </summary>
+public class SMSsender
+{
+    public bool SendMySMS(string msg, string mobile)
+    {
+        string userid = "PNPLRC";
+        string pass = "pnplrc@5001";
+        string route = "205";
+        string senderid = "PNPLRC";
+        string APIUrl = "http://sendsms.sandeshwala.com/API/WebSMS/Http/v1.0a/index.php?username=" + userid + "&password=" + pass + "&sender=" + senderid + "&to=" + mobile + "&message=" + msg + "&reqid=1&format={json|text}&route_id=" + route;
+
+        WebClient wc = new WebClient();
+        string result = wc.DownloadString(APIUrl);
+        if (result != "")
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
