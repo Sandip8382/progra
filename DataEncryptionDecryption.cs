a9fd1d1799b1a03b251090f5367838e465fefa09
Sandip8382 <40880777+Sandip8382@users.noreply.github.com>
2018-07-25 10:11:14 +0530

    Add files via upload

---
+++ DataEncryptionDecryption.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for DataEncryptionDecryption
+/// </summary>
+public class DataEncryptionDecryption
+{
+    public string EncryptMyData(string str)
+    {
+        string newstring = "";
+        int ascii;
+        foreach (char ch in str)
+        {
+            ascii = ch;
+            ascii = ascii + 5;
+            newstring = newstring + (char)ascii;
+        }
+        return newstring;
+    }
+    
+    public string DecryptMyData(string str)
+    {
+        string newstring = "";
+        int ascii;
+        foreach (char ch in str)
+        {
+            ascii = ch;
+            ascii = ascii - 5;
+            newstring = newstring + (char)ascii;
+        }
+        return newstring;
+    }
+}
---
+++ EmailSender.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net;  /// for network connect
+using System.Net.Mail; /// for mail SMTP server create
+
+/// <summary>
+/// Summary description for EmailSender
+/// </summary>
+public class EmailSender
+{
+    public bool SendMyEmail(string to, string subject, string message)
+    {
+        string username = "sandipkumargzp@gmail.com";
+        string password="8948656462";
+        SmtpClient Client = new SmtpClient();
+        MailMessage msg = new MailMessage();
+        MailAddress from = new MailAddress(username);
+        MailAddress sendto = new MailAddress(to);
+        //SMTP SETTING
+        NetworkCredential nc = new NetworkCredential(username, password);
+        Client.EnableSsl = true;
+        Client.Host = "smtp.gmail.com";
+        Client.UseDefaultCredentials = false;
+        Client.Port = 587;
+        Client.Credentials = nc;
+
+        // mail message settings
+
+        msg.IsBodyHtml = true;
+        msg.Sender = from;
+        msg.From = from;
+        msg.Subject = subject;
+        msg.To.Add(sendto);
+        msg.Body = message;
+
+        Client.Send(msg);
+        return true;
+
+
+    }
+
+}
